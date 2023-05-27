ee6510e8e549b152daa79092e1d3a9594bbd8623
Hari kowshik Molugu <104977337+HarikowshikMolugu@users.noreply.github.com>
2023-05-27 21:46:10 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -9,7 +9,7 @@ var usersRouter = require('./routes/users');
 testAPIRouter= require("./routes/testAPI");
 var app = express();
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
